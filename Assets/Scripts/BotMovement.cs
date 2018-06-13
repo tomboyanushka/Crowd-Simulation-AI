34d3f65730f131fc7dd960a7c7a4aaa72b3ef4f5
Anushka C Nair (RIT Student) <acn3929@ad.rit.edu>
2018-06-13 17:42:23 -0400

    Initial Commit

---
+++ Assets/Scripts/BotMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BotMovement : MonoBehaviour
+{
+    public Transform home;
+    NavMeshAgent agent;
+
+
+	void Start ()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(home.position);
+	}
+}
