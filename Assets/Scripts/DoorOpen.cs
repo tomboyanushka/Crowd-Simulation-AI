f4107b478818e1af5b519fbcd48e207b0e7108b4
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-06-14 01:04:04 -0400

    More changes

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    bool isDoorOpen = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isDoorOpen)
+                this.transform.Translate(0, -4, 0);
+            else
+                this.transform.Translate(0, 4, 0);
+            isDoorOpen = !isDoorOpen;
+        }
+            
+       
+	}
+}
